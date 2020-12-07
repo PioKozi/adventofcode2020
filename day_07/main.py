59f6a5abbc613c9f28d840f620f69432c29b29be
piokozi <47647338+PioKozi@users.noreply.github.com>
2020-12-07 23:27:34 +0000

    day 7

---
+++ day_07/main.py
@@ -0,0 +1,44 @@
+import re
+
+inputfile = open("./input", "r")
+
+contained_by = dict()
+must_contain = dict()
+
+# make the map which maps a colour to what it can contain
+for line in inputfile:
+    line = line.strip()
+    relevant_bags = list()
+    parts = line.split("contain")
+    parts += parts.pop(1).split(",")
+    parts = [part.replace("bags", "").replace("bag", "").strip(" .") for part in parts]
+    contained_by[parts[0]] = [i for i in parts[1:] if i != "no other"]
+
+# make the map which maps colour to what it can be contained by (reverse of above)
+for i in contained_by:
+    possible = list()
+    for j in contained_by:
+        if i in [re.sub("\d+ ", "", k) for k in contained_by[j]]: # I hate this
+            possible.append(j)
+    must_contain[i] = possible
+
+# Part 1
+possible = set()
+def traverse(possible, current):
+    for i in must_contain[current]:
+        possible.add(i)
+        traverse(possible, i)
+    return possible
+possible = traverse(possible, "shiny gold")
+print(len(possible))
+
+# Part 2
+required = 0
+def traverse(required, current):
+    for i in contained_by[current]:
+        required += int(re.search("\d+", i).group())
+        for j in range(int(re.search("\d+", i).group())): # per required bag
+            required = traverse(required, re.sub("\d+ ", "", i))
+    return required
+required = traverse(required, "shiny gold")
+print(required)
